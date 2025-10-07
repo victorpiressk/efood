4519c13ee87095a1b1897d8cde81334a7ef019e3
victorpiressk <victorpiressk@gmail.com>
2025-10-07 12:43:28 -0300

    atualização url api

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 const api = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://ebac-fake-api.vercel.app/api/efood'
+    baseUrl: 'https://api-ebac.vercel.app/api/efood'
   }),
   endpoints: (builder) => ({
     getRestaurants: builder.query<Food[], void>({
